4ab42905830a95dadf6a366a6c290bb57853c792
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-03-21 11:49:07 +0300

    Fetched data from an API where it returns a list of movies

--- src/searchmovies.js
+++ src/searchmovies.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 export default function SearchMovies() {
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("Submitting");
+
+        const query = "Jurassic Park";
+
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=8752db6ca168ae4b1865aeac42f13afc&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        } catch(err) {
+            console.error(err);
+        }
+
+    }
 
     return (
-       <form className="form">
+       <form className="form" onSubmit={searchMovies}>
            <label htmlFor="query" className="label"> Movie Name</label>
            <input className="input" type="text" placeholder="Search for a movie" name="query"/>
            <button className="button" type="submit">Search</button>
